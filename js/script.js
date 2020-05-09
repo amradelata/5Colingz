dd07602d906fdd788f4af1b83ef5e6416ad17c29
amradelata <amradelata@gmail.com>
2020-05-09 04:14:42 +0200

    master

--- js/script.js
+++ js/script.js
@@ -29,3 +29,21 @@
 		}
 	});
 // end fixed menu
+
+
+/* popup*/
+let mypopup = document.getElementById('mypopup');
+function closepopup(){
+	mypopup.style.display = "none";
+}
+	function shwpopup() {
+	setTimeout(function(){ 
+
+		mypopup.style.display = "block";
+
+	 }, 3000);
+	}
+
+	shwpopup() 
+
+	/* popup*/
